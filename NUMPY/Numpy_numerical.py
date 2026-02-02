e09a9505193e5b2dda29d99d5243a4f1e9f0df96
ilangovgu <ilangovgu@gmail.com>
2026-02-02 23:24:38 +0400

    add numerical numpy

---
+++ NUMPY/Numpy_numerical.py
@@ -0,0 +1,11 @@
+# Numpy numerical operations
+
+import numpy as np
+radar_readings=np.array([0.2,0.5,9,5,4.3,5.8,2,4.0,6,8,3.4]) #in meters
+
+print(np.sqrt(radar_readings))
+print(np.round(radar_readings))
+print(np.ceil(radar_readings))
+print(np.floor(radar_readings))
+print(np.pi)
+print(np.exp(radar_readings))
